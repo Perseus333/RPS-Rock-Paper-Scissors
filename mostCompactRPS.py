065198d3e745211b7c94cc2d82d2c2ed8b4692a3
Perseus-Lynx <perseusmith73@gmail.com>
2021-08-21 22:11:48 +0200

    Even more compact
    Removed spaces and assigned 3 variables in the same line

--- mostCompactRPS.py
+++ mostCompactRPS.py
@@ -1,7 +1,5 @@
 import random
-table = {'r': 1, 'p': 2, 's': 3}
-machine_attack = random.choice(['r', 'p', 's'])
-player_attack = input('> ')
-if table[player_attack] > table[machine_attack] or player_attack == 'r' and machine_attack == 's': print(f'{machine_attack}, you win')
-elif player_attack == machine_attack: print('draw')
-else: print(f'{machine_attack} you lose')
+machine_attack,player_attack,table=random.choice(['r','p','s']),input('>'),{'r':1,'p':2,'s':3}
+if table[player_attack]>table[machine_attack]or player_attack=='r'and machine_attack=='s':print(f'{machine_attack},win')
+elif player_attack==machine_attack:print('draw')
+else:print(f'{machine_attack},lose')
