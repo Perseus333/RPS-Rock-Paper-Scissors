963a90e955b68a2dd69947a2bb886e298ca9c04b
Perseus-Lynx <perseusmith73@gmail.com>
2021-08-22 15:54:14 +0200

    Symplifing and correcting the if statement
    Made the code more comapct by:
    -removing inecessary spaces
    -shortening to i¡one character the variables
    -fixing the if statement, that didn't work with a certain type of combination
    
    -changing the win lose or draw to w l or d

--- mostCompactRPS.py
+++ mostCompactRPS.py
@@ -1,5 +1,5 @@
-import random
-machine_attack,player_attack,table=random.choice(['r','p','s']),input('>'),{'r':1,'p':2,'s':3}
-if table[player_attack]>table[machine_attack]or player_attack=='r'and machine_attack=='s':print(f'{machine_attack},win')
-elif player_attack==machine_attack:print('draw')
-else:print(f'{machine_attack},lose')
+import random as r
+m,p,t=r.choice(['r','p','s']),input(),{'r':1,'p':2,'s':3}
+if t[p]-t[m]==1:print(m+'w')
+elif p==m:print('d')
+else:print(m+'l')
